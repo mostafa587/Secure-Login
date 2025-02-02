6764228af4b09fa1692c16143ab0d289a0b1ee4c
Mostafa <mostafaelbadawy5@gmail.com>
2025-02-02 06:45:33 +0200

    complete correct login

--- login/views.py
+++ login/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from signup.models import User
 
 # Create your views here.
 
 
 def login(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        if not username or not password:
+            return render(request, 'login/login.html', {'error': 'All fields are required'})
+        if not User.objects.filter(username=username).exists():
+            return render(request, 'login/login.html', {'error': 'Username does not exist'})
+        elif not User.objects.filter(password=password).exists():
+            return render(request, 'login/login.html', {'error': 'Password is incorrect'})
+        else:
+            return HttpResponse('Login successful')
     return render(request, 'login/login.html')
 
