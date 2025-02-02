c4118784a0c37d47fef279b1661021d5a4246909
Mostafa <mostafaelbadawy5@gmail.com>
2025-02-02 06:33:49 +0200

    complete correct regestiration

--- signup/admin.py
+++ signup/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import User
 # Register your models here.
+
+admin.site.register(User) # This will allow the User model to be managed from the admin panel
--- signup/views.py
+++ signup/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . import models
 
 # Create your views here.
 
@@ -13,4 +14,15 @@ def signup(request):
         phone = request.POST['phone'] # Raises KeyError if "phone" is missing in the html form
         if not username or not password or not email or not phone: #could if username == None or password == None or email == None or phone == None:
             return render(request, 'signup/signup.html', {'error': 'All fields are required'})
+        
+        if models.User.objects.filter(username=username).exists():
+            return render(request, 'signup/signup.html', {'error': 'Username already exists'}) 
+        elif models.User.objects.filter(email=email).exists():
+            return render(request, 'signup/signup.html', {'error': 'Email already exists'})
+        elif models.User.objects.filter(phone=phone).exists():
+            return render(request, 'signup/signup.html', {'error': 'Phone already exists'})
+        else:
+            models.User.objects.create(username=username, password=password, email=email, phone=phone)
+            return HttpResponse('User created successfully')
+        
     return render(request, 'signup/signup.html')
